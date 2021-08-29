63fa12a4123e33826f4e2e79504f58c2735e3181
admin <tungdoiroi@gmail.com>
2021-08-29 20:09:56 +0700

    tja

---
+++ src/main/java/dao/DAO.java
@@ -0,0 +1,49 @@
+package dao;
+
+import context.DBContext;
+import entity.Product;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class DAO {
+
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    public List<Product> getAllProduct() {
+        List<Product> list = new ArrayList<>();
+        String query = "select * from product";
+        try {
+            conn = new DBContext().getConnection();//mo ket noi voi sql
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5),
+                        rs.getString(6)));
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        DAO dao = new DAO();
+        List<Product> list = dao.getAllProduct();
+        List<Product> listC = dao.getAllProduct();
+
+        for (Product o : listC) {
+            System.out.println(o);
+        }
+
+    }
+}
